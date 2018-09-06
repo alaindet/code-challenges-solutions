b4b6c9e8104433c80edf7f0b8281f542797e948c
Alain D'Ettorre <alain.det@gmail.com>
2018-09-06 15:38:27 +0200

    Add .../7kyu/odd-or-even.js

---
+++ solutions/javascript/7kyu/odd-or-even.js
@@ -0,0 +1,15 @@
+/**
+ * Given an array of numbers(a list in groovy), determine whether
+ * the sum of all of the numbers is odd or even.
+ * Give your answer in string format as 'odd' or 'even'.
+ * If the input array is empty consider it as: [0](array with a zero).
+ * 
+ * @param array numbers 
+ * @return string 'even' or 'odd'
+ */
+const oddOrEven = (numbers) => {
+  return (numbers||[0]).reduce((s, n) => s+=n, 0)%2===0 ? 'even' : 'odd';
+};
+
+// Testing
+console.log(oddOrEven([2, 5, 34, 6]));
