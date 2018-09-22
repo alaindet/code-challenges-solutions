e1ef07f1cd0a6dd32837ca01d954d620457bcb79
Alain D'Ettorre <alain.det@gmail.com>
2018-09-22 12:17:42 +0200

    Fix assertions for BackwardsReadPrimesTest.php

--- solutions/php/tests/6kyu/BackwardsReadPrimesTest.php
+++ solutions/php/tests/6kyu/BackwardsReadPrimesTest.php
@@ -13,12 +13,12 @@ class BackwardsReadPrimesTest extends \PHPUnit\Framework\TestCase
         );
 
         $this->assertEquals(
-            [70001, 70009, 70061, 70079, 70121, 70141, 70163, 70241],
+            [70001, 70009, 70061, 70079, 70121, 70141, 70163, 70207, 70241],
             backwardsPrime(70000, 70245)
         );
 
         $this->assertEquals(
-            [70489, 70529, 70573, 70589],
+            [70489, 70507, 70529, 70573, 70589],
             backwardsPrime(70485, 70600)
         );
     }
