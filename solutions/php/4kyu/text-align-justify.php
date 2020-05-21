6831e42ca125c61d01adfce6c39b52d6159305be
Alain D'Ettorre <alain.det@gmail.com>
2020-05-21 18:22:52 +0200

    Add proposed solution to 4kyu/text-align-justify.php

--- solutions/php/4kyu/text-align-justify.php
+++ solutions/php/4kyu/text-align-justify.php
@@ -17,11 +17,78 @@
  * - Strings with one word do not need gaps ('somelongword\n').
  */
 
-function justify(string $str, int $len)
+/**
+ * Returns an array of lines according to the chars limit of each line
+ * 
+ * Ex.:
+ * [
+ *   ['Hello', 'word...'],
+ *   ['How', 'are', 'you?'],
+ * ]
+ * 
+ * @param string $input
+ * @param int $limit
+ */
+function chopLines(string $input, int $limit): array
 {
-    return null;
+    $words = explode(' ', $input);
+
+    $lines = [];
+    $line = [];
+    $chars = 0;
+
+    foreach ($words as $word) {
+        $len = strlen($word);
+
+        if ($chars + $len > $limit) {
+            $lines[] = $line;
+            $line = [];
+            $chars = 0;
+        }
+
+        $line[] = $word;
+        $chars += $len + 1;
+    }
+
+    $lines[] = $line;
+
+    return $lines;
 }
 
+function stretchWhitespace(array $words, int $limit): string
+{
+    $result = '';
+    $currentLength = strlen(implode(' ', $words));
+    $diff = $limit - $currentLength;
+    $spaces = count($words) - 1;
+    $min = $spaces === 0 ? 0 : intval($diff / $spaces);
+    $rest = $spaces === 0 ? 0 : $diff % $spaces;
+
+    foreach ($words as $word) {
+        $minSpace = str_repeat(' ', $min);
+        $spareSpace = '';
+        if ($rest-- > 0) $spareSpace = ' ';
+        $result .= "${word} ${minSpace}${spareSpace}";
+    }
+
+    return rtrim($result);
+}
+
+function justify(string $input, int $limit): string
+{
+    $stretchedLines = [];
+    $lines = chopLines($input, $limit);
+    $linesCount = count($lines);
+    
+    for ($i = 0; $i < $linesCount; $i++) {
+        $line = $lines[$i];
+        $last = $i === $linesCount - 1;
+        $line = $last ? implode(' ', $line) : stretchWhitespace($line, $limit);
+        $stretchedLines[] = $line;
+    }
+
+    return implode("\n", $stretchedLines);
+}
 
 $tests = [
 
@@ -31,7 +98,7 @@ $tests = [
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna.',
             30,
         ],
-        'expected' => "Lorem  ipsum  dolor  sit amet,\nconsectetur  adipiscing  elit.Vestibulum    sagittis   dolor\nmauris,  at  elementum  ligula\ntempor  eget.  In quis rhoncus\nnunc,  at  aliquet orci. Fusce\nat   dolor   sit   amet  felissuscipit   tristique.   Nam  a\nimperdiet   tellus.  Nulla  eu\nvestibulum urna."
+        'expected' => "Lorem  ipsum  dolor  sit amet,\nconsectetur  adipiscing  elit.\nVestibulum    sagittis   dolor\nmauris,  at  elementum  ligula\ntempor  eget.  In quis rhoncus\nnunc,  at  aliquet orci. Fusce\nat   dolor   sit   amet  felis\nsuscipit   tristique.   Nam  a\nimperdiet   tellus.  Nulla  eu\nvestibulum urna."
     ],
 
     // Test #2
@@ -46,8 +113,22 @@ $tests = [
 ];
 
 foreach ($tests as $test) {
-    [$given, $expected] = [$test['given'], $test['expected']];
-    $assertion = justify(...$given);
-    echo $assertion === $expected ? 'PASSED' : 'FAILED';
-    echo "\n";
+    $assertion = justify(...$test['given']);
+    $outcome = $assertion === $test['expected'];
+
+    if (!$outcome) {
+        echo implode("\n", [
+            "\n",
+            'Assertion:',
+            print_r($assertion, true),
+            "\n\n",
+            'Expected:',
+            print_r($test['expected'], true),
+            "\n",
+            'FAILED',
+        ]);
+        continue;
+    }
+
+    echo "PASSED\n";
 }
