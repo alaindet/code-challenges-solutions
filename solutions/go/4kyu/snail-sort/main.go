123ceac8d3cb372f4bbf31a1bba8d5b6d4ccb718
Alain D'Ettorre <alain.det@gmail.com>
2022-05-16 09:42:21 +0200

    Add working wolution for Go 4kyu/snail-sort

--- solutions/go/4kyu/snail-sort/main.go
+++ solutions/go/4kyu/snail-sort/main.go
@@ -1,23 +1,127 @@
 package main
 
-import "fmt"
-
-func main() {
-	input := [][]int{
-		{2, 3, 4, 6, 7},
-		{3, 4, 2, 1, 5},
-		{9, 7, 4, 5, 8},
-		{3, 5, 2, 6, 7},
-		{1, 0, 5, 7, 4},
-	}
-
-	result := Snail(input)
-
-	fmt.Println("result", result)
-}
-
 // https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/go
 func Snail(snailMap [][]int) []int {
 	s := NewSnailSort(snailMap)
 	return s.Sort()
+}
+
+type SnailSort struct {
+	grid                 [][]int
+	width                int
+	height               int
+	visitedGrid          [][]bool
+	directions           [][]int
+	currentDirection     []int
+	directionJustChanged bool
+	directionIndex       int
+	currentPosition      []int
+	nextPosition         []int
+	result               []int
+}
+
+// X-axis => vertical, positive down
+// Y-axis => horizontal, positive right
+func NewSnailSort(grid [][]int) *SnailSort {
+	s := &SnailSort{}
+	s.grid = grid
+	s.height = len(grid)
+	s.width = len(grid[0])
+	s.visitedGrid = s.initVisitedGrid()
+	s.directions = [][]int{
+		{0, 1},  // Right
+		{1, 0},  // Down
+		{0, -1}, // Left
+		{-1, 0}, // Up
+	}
+	s.currentPosition = []int{0, 0}
+	s.currentDirection = nil
+	s.nextPosition = nil
+	s.result = make([]int, 0, s.width*s.height)
+	return s
+}
+
+func (s *SnailSort) initVisitedGrid() [][]bool {
+	visited := make([][]bool, 0, s.height)
+	for i := 0; i < s.height; i++ {
+		visited = append(visited, make([]bool, s.width))
+	}
+	return visited
+}
+
+func (s *SnailSort) calculateNextPosition() []int {
+	s.nextPosition = []int{
+		s.currentPosition[0] + s.currentDirection[0],
+		s.currentPosition[1] + s.currentDirection[1],
+	}
+
+	return s.nextPosition
+}
+
+func (s *SnailSort) isPosition(pos []int) bool {
+
+	x, y := pos[0], pos[1]
+
+	// Vertically out of bound?
+	if x < 0 || x >= s.height {
+		return false
+	}
+
+	// Horizontally out of bound?
+	if y < 0 || y >= s.width {
+		return false
+	}
+
+	// Already visited?
+	if s.visitedGrid[x][y] {
+		return false
+	}
+
+	return true
+}
+
+func (s *SnailSort) changeDirection() {
+	s.directionJustChanged = true
+	s.directionIndex += 1
+	if s.directionIndex >= len(s.directions) {
+		s.directionIndex -= len(s.directions)
+	}
+	s.currentDirection = s.directions[s.directionIndex]
+}
+
+func (s *SnailSort) changePosition() {
+	x, y := s.nextPosition[0], s.nextPosition[1]
+	s.currentPosition = []int{x, y}
+	s.visitedGrid[x][y] = true
+	s.result = append(s.result, s.grid[x][y])
+	s.directionJustChanged = false
+}
+
+func (s *SnailSort) Sort() []int {
+
+	if len(s.grid[0]) == 0 {
+		return []int{}
+	}
+
+	s.visitedGrid[0][0] = true
+	s.directionIndex = 0
+	s.currentDirection = s.directions[0]
+	s.currentPosition = []int{0, 0}
+	s.directionJustChanged = false
+	s.result = []int{s.grid[0][0]}
+
+	for {
+		s.visitedGrid[0][0] = true
+		if !s.isPosition(s.calculateNextPosition()) {
+			if s.directionJustChanged {
+				break
+			}
+
+			s.changeDirection()
+			continue
+		}
+		s.changePosition()
+	}
+
+	return s.result
 }
