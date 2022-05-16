1997036c7a66703741dee5f30ea6f874420fdc98
Alain D'Ettorre <alain.det@gmail.com>
2022-05-16 09:07:55 +0200

    WIP: Go 4kyu/snail-sort

--- solutions/go/4kyu/snail-sort/main.go
+++ solutions/go/4kyu/snail-sort/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
+func main() {
+	input := [][]int{
+		{2, 3, 4, 6, 7},
+		{3, 4, 2, 1, 5},
+		{9, 7, 4, 5, 8},
+		{3, 5, 2, 6, 7},
+		{1, 0, 5, 7, 4},
+	}
+
+	result := Snail(input)
+
+	fmt.Println("result", result)
+}
+
 // https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/go
 func Snail(snailMap [][]int) []int {
-	// TODO
-	return []int{1, 2, 3, 6, 9, 8, 7, 4, 5}
+	s := NewSnailSort(snailMap)
+	return s.Sort()
 }
