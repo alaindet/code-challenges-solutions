7d418a7297acd0472319fc79b807e5a5575ed98e
Alain D'Ettorre <alain.det@gmail.com>
2022-05-21 15:34:28 +0200

    Update solution for "Trapping rain water" challenge on leetcode.com

--- leetcode.com/go/trapping-rain-water/main.go
+++ leetcode.com/go/trapping-rain-water/main.go
@@ -1,52 +1,89 @@
 package main
 
 // https://leetcode.com/problems/trapping-rain-water/
+// This is a fast solution but still fails for big arrays (N: 20k, max val: 100k)
 func trap(height []int) int {
 	water := 0
-	size := len(height)
+	lineCh := make(chan []bool)
+	waterCh := make(chan int)
 
-	for {
-		waterSegment := 0
-		allSubmerged := true
-
-		for i := 0; i < size; i++ {
-
-			// Is it a rock?
-			if height[i] > 0 {
-				water += waterSegment
-				waterSegment = 0
-				allSubmerged = false
-				height[i]-- // Sink
-				continue
+	// Generate lines
+	go func(ch chan []bool) {
+		threshold := 0
+		for {
+			if line, done := generateLine(&height, threshold); done {
+				break
+			} else {
+				ch <- *line
+				threshold++
 			}
-
-			// Is it an air cell on the edge?
-			if i == 0 || i == (size-1) {
-				waterSegment = 0
-				height[i]-- // Sink
-				continue
-			}
-
-			// Is it an air cell with a left (sunken) rock?
-			if height[i-1] >= 0 {
-				waterSegment = 1
-				height[i]--
-				continue
-			}
-
-			// Is it just an air cell?
-			if waterSegment > 0 {
-				waterSegment++
-			}
-
-			// Sink the cell
-			height[i]--
 		}
+		close(lineCh)
+	}(lineCh)
 
-		if allSubmerged {
-			break
+	// Count water cells in lines
+	go func(inputCh chan []bool, outputCh chan int) {
+		for line := range lineCh {
+			waterCh <- countWaterInLine(line)
 		}
+		close(waterCh)
+	}(lineCh, waterCh)
+
+	for waterCount := range waterCh {
+		water += waterCount
 	}
 
 	return water
+}
+
+func generateLine(arr *[]int, threshold int) (*[]bool, bool) {
+	line := make([]bool, len(*arr))
+	allBelow := true
+	for i, val := range *arr {
+		if val > threshold {
+			line[i] = true
+			allBelow = false
+		}
+	}
+
+	if allBelow {
+		return nil, true
+	}
+
+	return &line, false
+}
+
+func countWaterInLine(line []bool) int {
+	waterInLine := 0
+	size := len(line)
+	waterSegment := 0
+
+	for i, cell := range line {
+
+		// Is it a rock cell?
+		if cell {
+			waterInLine += waterSegment
+			waterSegment = 0
+			continue
+		}
+
+		// Is it an air cell on the edge?
+		if i == 0 || i == (size-1) {
+			waterSegment = 0
+			continue
+		}
+
+		// Is it an air cell with a left rock cell?
+		if line[i-1] {
+			waterSegment = 1
+			continue
+		}
+
+		// Is it just an air cell surrounded by air cells?
+		if waterSegment > 0 {
+			waterSegment++
+		}
+	}
+
+	return waterInLine
 }
