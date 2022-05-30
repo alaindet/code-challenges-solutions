d6ac66ea768bcc1efca287afc102a93db9413021
Alain D'Ettorre <alain.det@gmail.com>
2022-05-30 18:40:08 +0200

    Add Go: 5kyu/iterative-rotation-cipher

--- codewars.com/go/5kyu/iterative-rotation-cipher/main.go
+++ codewars.com/go/5kyu/iterative-rotation-cipher/main.go
@@ -1,36 +1,163 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-/*
-Encode(10, "If you wish to make an apple pie from scratch, you must first invent the universe.")
+const (
+	space = 32
+)
 
-Step 1 — remove all spaces:
-`Ifyouwishtomakeanapplepiefromscratch,youmustfirstinventtheuniverse.`
-
-Step 2 — shift the order of string characters to the right by 10:
-`euniverse.Ifyouwishtomakeanapplepiefromscratch,youmustfirstinventth`
-
-Step 3 — place the spaces back in their original positions:
-`eu niv erse .I fyou wi shtom ake anap plepiefr oms crat ch,yo umustf irs tinventth`
-
-Step 4 — shift the order of characters for each space-separated substring to the right by 10:
-`eu vni seer .I oufy wi shtom eak apan frplepie som atcr ch,yo ustfum sir htinventt`
-
-Step 5 - Repeat the steps 9 more times before returning the string with `10 ` prepended.
-*/
-
-// https://www.codewars.com/kata/5a3357ae8058425bde002674/train/go
 func Encode(n int, s string) string {
-	return ""
+	result := s
+	for i := 0; i < n; i++ {
+		result = _encode(n, result)
+	}
+	return fmt.Sprintf("%d %s", n, result)
 }
 
 func Decode(s string) string {
-	return ""
+	n, encoded := parseEncoded(s)
+	result := encoded
+	for i := 0; i < n; i++ {
+		result = _decode(n, result)
+	}
+	return result
 }
 
-func main() {
-	fmt.Println(
-		Encode(10, "If you wish to make an apple pie from scratch, you must first invent the universe."),
-	)
+func _encode(n int, s string) string {
+	letters := []rune(s)
+	count := len(letters)
+	wordLength := 0
+	wordLengths := make([]int, 0, count)
+	condensed := make([]rune, 0, count)
+
+	for _, letter := range letters {
+		if letter == space {
+			wordLengths = append(wordLengths, wordLength)
+			wordLength = 0
+		} else {
+			wordLength++
+			condensed = append(condensed, letter)
+		}
+	}
+
+	wordLengths = append(wordLengths, wordLength)
+	rotateRight(n, &condensed)
+	words := make([][]rune, 0, len(wordLengths)+1)
+
+	from := 0
+	for _, wl := range wordLengths {
+		slice := condensed[from : from+wl]
+		word := append(make([]rune, 0, len(slice)), slice...)
+		words = append(words, word)
+		from += wl
+	}
+
+	var b strings.Builder
+
+	for wi := range words {
+		rotateRight(n, &words[wi])
+		b.WriteString(string(words[wi]))
+		b.WriteRune(space)
+	}
+
+	result := b.String()
+
+	return result[:len(result)-1]
+}
+
+func _decode(n int, s string) string {
+
+	count := len(s)
+	words := make([][]rune, 0, count)
+	word := make([]rune, 0, count)
+	wordLengths := make([]int, 0, count)
+
+	for _, r := range []rune(s) {
+		switch {
+		case r == space:
+			words = append(words, word)
+			word = make([]rune, 0, len(s))
+		default:
+			word = append(word, r)
+		}
+	}
+	words = append(words, word)
+
+	condensed := make([]rune, 0, count)
+
+	for _, w := range words {
+		wordLengths = append(wordLengths, len(w))
+		rotateLeft(n, &w)
+		condensed = append(condensed, w...)
+	}
+
+	rotateLeft(n, &condensed)
+
+	from := 0
+	var b strings.Builder
+	for _, wl := range wordLengths {
+		b.WriteString(string(condensed[from : from+wl]))
+		b.WriteRune(space)
+		from += wl
+	}
+
+	result := b.String()
+
+	return result[:len(result)-1]
+}
+
+func parseEncoded(s string) (int, string) {
+
+	chars := []rune(s)
+	pos := 0
+
+	for i, r := range chars {
+		if r == space {
+			pos = i
+			break
+		}
+	}
+
+	n, _ := strconv.Atoi(string(chars[:pos]))
+	encoded := string(chars[pos+1:])
+
+	return n, encoded
+}
+
+func rotateRight[K any](n int, ref *[]K) {
+	arr := *ref
+	count := len(arr)
+
+	if count == 0 {
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		last := arr[count-1]
+		for i := count - 2; i >= 0; i-- {
+			arr[i+1] = arr[i]
+		}
+		arr[0] = last
+	}
+}
+
+func rotateLeft[K any](n int, ref *[]K) {
+	arr := *ref
+	count := len(arr)
+
+	if count == 0 {
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		first := arr[0]
+		for i := 0; i < count-1; i++ {
+			arr[i] = arr[i+1]
+		}
+		arr[count-1] = first
+	}
 }
