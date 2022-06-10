c8d4b39b82f3bd7b40fc04fe5c06d594c6bc4658
Alain D'Ettorre <alain.det@gmail.com>
2022-06-10 16:28:19 +0200

    Add better solution for Go 4kyu/how-many-numbers

--- codewars.com/go/4kyu/how-many-numbers/main.go
+++ codewars.com/go/4kyu/how-many-numbers/main.go
@@ -1,46 +1,102 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+const (
+	routines = 8
+)
+
+func main() {
+	// 118, 127, 136, 145, 226, 235, 244, 334
+	fmt.Println("FindAll(10,3)", FindAll(10, 3))
+	fmt.Println("FindAll(84, 4)", FindAll(84, 4))
+}
+
 // WARNING: Does not work for big numbers
 // https://www.codewars.com/kata/5877e7d568909e5ff90017e6/train/go
 func FindAll(digitsSum, digitsCount int) []int {
 
-	var count, lowest, highest int
-	max := 1
+	toProcess := make(chan []int, routines)
+	filtered := make(chan []int, routines)
+	aggregated := make(chan []int)
 
+	max := 1
 	for d := 1; d <= digitsCount; d++ {
 		max *= 10
 	}
 
-	// Consider only numbers with same digits count
-	for i := max / 10; i < max; i++ {
+	min := max / 10
+	delta := (max - min + 1) / routines
 
-		if !isValid(i, digitsCount, max, digitsSum) {
-			continue
+	// Feed the chunks
+	go func(ch chan []int) {
+		inf := min
+		for i := 0; i < routines; i++ {
+			if i > 0 {
+				inf += delta + 1
+			}
+			sup := inf + delta
+
+			if sup > max {
+				sup = max
+			}
+
+			ch <- []int{inf, sup}
+		}
+		close(ch)
+	}(toProcess)
+
+	// Filter the chunks
+	go func(fromCh, toCh chan []int, digitsCount, digitsSum, max int) {
+		var wg sync.WaitGroup
+		wg.Add(routines)
+		for limits := range fromCh {
+			go func(limits []int) {
+				defer wg.Done()
+				inf, sup := limits[0], limits[1]
+				nums := make([]int, 0, (sup-inf)/10)
+				for i := inf; i <= sup; i++ {
+					if isValid(i, digitsCount, max, digitsSum) {
+						nums = append(nums, i)
+					}
+				}
+				toCh <- nums
+			}(limits)
+		}
+		wg.Wait()
+		close(toCh)
+	}(toProcess, filtered, digitsCount, digitsSum, max)
+
+	// Aggregate filtered data
+	go func(fromCh, toCh chan []int) {
+		var count, lowest, highest int
+		lowest = max
+		highest = 0
+		for nums := range fromCh {
+			count += len(nums)
+			for _, n := range nums {
+				if n < lowest {
+					lowest = n
+				}
+				if n > highest {
+					highest = n
+				}
+			}
 		}
 
-		if count == 0 {
-			lowest = i
-			highest = i
+		result := make([]int, 0, 3)
+		if count > 0 {
+			result = []int{count, lowest, highest}
 		}
+		toCh <- result
+		close(toCh)
+	}(filtered, aggregated)
 
-		count++
-
-		if i < lowest {
-			lowest = i
-		}
-
-		if i > highest {
-			highest = i
-		}
-
-		i++
-	}
-
-	if count == 0 {
-		return []int{}
-	}
-
-	return []int{count, lowest, highest}
+	result := <-aggregated
+	return result
 }
 
 func isValid(n, nLength, max, digitsSum int) bool {
