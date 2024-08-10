701cef2c4e8ae812f3ab12539522f432d47d90d4
Alain D'Ettorre <alain.det@gmail.com>
2024-08-10 09:51:29 +0200

    Add Python codewars.com/7kyu/disemvowel-trolls.py

---
+++ codewars.com/python/7kyu/disemvowel-trolls.py
@@ -0,0 +1,30 @@
+# https://www.codewars.com/kata/52fba66badcd10859f00097e/train/python
+
+vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+
+def disemvowel(input: str) -> str:
+    global vowels
+    return ''.join([c for c in input if c not in vowels])
+
+# -----------------------------------------------------------------------------
+
+testCases: list[tuple[str, str]] = [
+    (
+        'This website is for losers LOL!',
+        'Ths wbst s fr lsrs LL!',
+    ),
+    (
+        'No offense but,\nYour writing is among the worst I\'ve ever read',
+        'N ffns bt,\nYr wrtng s mng th wrst \'v vr rd',
+    ),
+    (
+        'What are you, a communist?',
+        'Wht r y,  cmmnst?',
+    ),
+]
+
+for testCase in testCases:
+    given, expected = testCase
+    result = disemvowel(given)
+    outcome = result == expected
+    print('PASSED' if outcome else 'NOT PASSED')
