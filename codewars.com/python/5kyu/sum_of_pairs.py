39976d27aef2cd82705f31fc6e2b4a2d409acd7e
Alain D'Ettorre <alain.det@gmail.com>
2024-08-14 23:50:08 +0200

    refactor: use unittest built-in module instead of custom testing code

--- codewars.com/python/5kyu/sum_of_pairs.py
+++ codewars.com/python/5kyu/sum_of_pairs.py
@@ -1,5 +1,8 @@
 # https://www.codewars.com/kata/54d81488b981293527000c8f/train/python
 
+import unittest
+
+
 def sum_pairs(numbers: list[int], target_sum: int) -> list[int, int]:
     """
     Thanks to
@@ -10,8 +13,6 @@ def sum_pairs(numbers: list[int], target_sum: int) -> list[int, int]:
     for right in numbers:
         left = target_sum - right
 
-        print(f'left={left:02} right={right:02}, visited={visited}')
-
         if left in visited:
             return [left, right]
 
@@ -19,115 +20,78 @@ def sum_pairs(numbers: list[int], target_sum: int) -> list[int, int]:
 
 # -----------------------------------------------------------------------------
 
-# Prepare a big list for stressing the function
-big_input_numbers = [1] * 10_000_000
-big_input_numbers[len(big_input_numbers) // 2 - 1] = 6
-big_input_numbers[len(big_input_numbers) // 2] = 7
-big_input_numbers[len(big_input_numbers) - 2] = 8
-big_input_numbers[len(big_input_numbers) - 1] = -3
-big_input_numbers[0] = 13
-big_input_numbers[1] = 3
+class TestSumPairs(unittest.TestCase):
 
-class SumPairsTestCase:
-    name: str
-    input_numbers: list[int]
-    input_sum: int
-    expected: list[int] | None
+    big_input_numbers: list[int]
 
-    def __init__(
-        self,
-        name: str,
-        input_numbers: list[int],
-        input_sum: int,
-        expected: list[int],
-    ):
-        self.name = name
-        self.input_numbers = input_numbers
-        self.input_sum = input_sum
-        self.expected = expected
+    def setUp(self):
+        self.big_input_numbers = [1] * 10_000_000
+        self.big_input_numbers[len(self.big_input_numbers) // 2 - 1] = 6
+        self.big_input_numbers[len(self.big_input_numbers) // 2] = 7
+        self.big_input_numbers[len(self.big_input_numbers) - 2] = 8
+        self.big_input_numbers[len(self.big_input_numbers) - 1] = -3
+        self.big_input_numbers[0] = 13
+        self.big_input_numbers[1] = 3
 
-    def run(self) -> None:
-        result = sum_pairs(self.input_numbers, self.input_sum)
-        assert result == self.expected, f'Expected: {self.expected}, got {result}'
+    def test_basic(self):
+        """Basic"""
+        result = sum_pairs([1, 4, 8, 7, 3, 15], 8)
+        self.assertListEqual(result, [1, 7])
 
-test_cases: list[SumPairsTestCase] = [
-    SumPairsTestCase(
-        name='Basic',
-        input_numbers=[1, 4, 8, 7, 3, 15],
-        input_sum=8,
-        expected=[1, 7],
-    ),
-    SumPairsTestCase(
-        name='Negatives',
-        input_numbers=[1, -2, 3, 0, -6, 1],
-        input_sum=-6,
-        expected=[0, -6],
-    ),
-    SumPairsTestCase(
-        name='No matches',
-        input_numbers=[20, -13, 40],
-        input_sum=-7,
-        expected=None,
-    ),
-    SumPairsTestCase(
-        name='First match from left #1',
-        input_numbers=[1, 2, 3, 4, 1, 0],
-        input_sum=2,
-        expected=[1, 1],
-    ),
-    SumPairsTestCase(
-        name='First match from left #2',
-        input_numbers=[10, 5, 2, 3, 7, 5],
-        input_sum=10,
-        expected=[3, 7],
-    ),
-    SumPairsTestCase(
-        name='Duplicates',
-        input_numbers=[4, -2, 3, 3, 4],
-        input_sum=8,
-        expected=[4, 4],
-    ),
-    SumPairsTestCase(
-        name='Zeroes',
-        input_numbers=[0, 2, 0],
-        input_sum=0,
-        expected=[0, 0],
-    ),
-    SumPairsTestCase(
-        name='Subtraction',
-        input_numbers=[5, 9, 13, -3],
-        input_sum=10,
-        expected=[13, -3],
-    ),
-    SumPairsTestCase(
-        name='Ten million numbers with middle pair',
-        input_numbers=big_input_numbers,
-        input_sum=13,
-        expected=[6, 7],
-    ),
-    SumPairsTestCase(
-        name='Ten million numbers with pair at end',
-        input_numbers=big_input_numbers,
-        input_sum=5,
-        expected=[8, -3],
-    ),
-    SumPairsTestCase(
-        name='Ten million numbers with pair at start',
-        input_numbers=big_input_numbers,
-        input_sum=16,
-        expected=[13, 3],
-    ),
-    SumPairsTestCase(
-        name='Ten million numbers with no match',
-        input_numbers=big_input_numbers,
-        input_sum=31,
-        expected=None,
-    ),
-]
+    def test_negatives(self):
+        """With negatives"""
+        result = sum_pairs([1, -2, 3, 0, -6, 1], -6)
+        self.assertListEqual(result, [0, -6])
 
-for test_case in test_cases:
-    try:
-        test_case.run()
-        print(f'OK: {test_case.name}')
-    except AssertionError as e:
-        print(f'Error: {test_case.name}: {e.__str__()}')
+    def test_no_matches(self):
+        """With no matches"""
+        result = sum_pairs([20, -13, 40], -7)
+        self.assertIsNone(result)
+
+    def test_first_match_left_1(self):
+        """With match match from left #1"""
+        result = sum_pairs([1, 2, 3, 4, 1, 0], 2)
+        self.assertListEqual(result, [1, 1])
+
+    def test_first_match_left_2(self):
+        """With match match from left #2"""
+        result = sum_pairs([10, 5, 2, 3, 7, 5], 10)
+        self.assertListEqual(result, [3, 7])
+
+    def test_duplicates(self):
+        """With match match from left #2"""
+        result = sum_pairs([4, -2, 3, 3, 4], 8)
+        self.assertListEqual(result, [4, 4])
+
+    def test_zeros(self):
+        """With zeros"""
+        result = sum_pairs([0, 2, 0], 0)
+        self.assertListEqual(result, [0, 0])
+
+    def test_subtraction(self):
+        """With subtraction"""
+        result = sum_pairs([5, 9, 13, -3], 10)
+        self.assertListEqual(result, [13, -3])
+
+    def test_ten_million_middle_pair(self):
+        """With ten million numbers with a pair in the middle"""
+        result = sum_pairs(self.big_input_numbers, 13)
+        self.assertListEqual(result, [6, 7])
+
+    def test_ten_million_end_pair(self):
+        """With ten million numbers with a pair at the end"""
+        result = sum_pairs(self.big_input_numbers, 5)
+        self.assertListEqual(result, [8, -3])
+
+    def test_ten_million_start_pair(self):
+        """With ten million numbers with a pair at the beginning"""
+        result = sum_pairs(self.big_input_numbers, 16)
+        self.assertListEqual(result, [13, 3])
+
+    def test_ten_million_no_match(self):
+        """With ten million numbers with no matches"""
+        result = sum_pairs(self.big_input_numbers, 31)
+        self.assertIsNone(result)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=1)
--- codewars.com/python/7kyu/remove_minimum.py
+++ codewars.com/python/7kyu/remove_minimum.py
@@ -1,46 +1,41 @@
 # https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/python
 
-from typing import List
+import unittest
 
-def remove_smallest(numbers: List[int]) -> List[int]:
+
+def remove_smallest(numbers: list[int]) -> list[int]:
 
     numbers_count = len(numbers)
 
     if numbers_count == 0:
         return []
-
-    smallest_value = min(numbers)
+    
+    min_value = min(numbers)
     result = []
 
     for index in range(numbers_count):
         current_value = numbers[index]
-        if current_value == smallest_value:
+        if current_value == min_value:
             break
         result.append(current_value)
 
     return result + numbers[index+1:]
 
 
-tests = (
-    {
-        'asserted': remove_smallest([1, 2, 3, 4, 5]),
-        'expected': [2, 3, 4, 5],
-    },
-    {
-        'asserted': remove_smallest([5, 3, 2, 1, 4]),
-        'expected': [5, 3, 2, 4],
-    },
-    {
-        'asserted': remove_smallest([1, 2, 3, 1, 1]),
-        'expected': [2, 3, 1, 1],
-    }
-)
+# -----------------------------------------------------------------------------
 
-def serialize_list(the_list: List) -> str:
-    the_list.sort()
-    return ','.join([str(x) for x in the_list])
+class TestRemoveSmallest(unittest.TestCase):
+    def test_with_ordered_sequence(self):
+        result = remove_smallest([1, 2, 3, 4, 5])
+        self.assertListEqual(result, [2, 3, 4, 5])
 
-for test in tests:
-    asserted = serialize_list(test['asserted'])
-    expected = serialize_list(test['expected'])
-    print('PASSED' if asserted == expected else 'NOT PASSED')
+    def test_with_random_sequence(self):
+        result = remove_smallest([5, 3, 2, 1, 4])
+        self.assertListEqual(result, [5, 3, 2, 4])
+
+    def test_with_repeating_minimum(self):
+        result = remove_smallest([1, 2, 3, 1, 1])
+        self.assertListEqual(result, [2, 3, 1, 1])
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
